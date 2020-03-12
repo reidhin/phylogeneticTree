06398b99ef16da28bbf2a8dfc15454033b62c08b
reidhin <hans@orikami.nl>
2020-03-12 07:56:10 +0100

    trying out Entrez

--- src/try_out.py
+++ src/try_out.py
@@ -1,5 +1,15 @@
 # libraries
+from Bio import Entrez
 
+Entrez.email = "hans@orikami.nl"  # Always tell NCBI who you are
+handle = Entrez.einfo()
+result = handle.read()
+handle = Entrez.esearch(db="nucleotide", term="Cypripedioideae[Orgn] AND matK[Gene]", idtype="acc")
+record = Entrez.read(handle)
+print(record["Count"])
+print(record["IdList"])
+handle.close()
+print('finished')
 # load relevant genetic data
 # from homo sapiens
 # from pan
