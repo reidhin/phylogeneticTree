ca1ffa5eec666b39d494914461dae9062d98fe06
reidhin <hans@orikami.nl>
2020-03-15 22:30:16 +0100

    downloading Corona RNA

---
+++ src/virus.py
@@ -0,0 +1,21 @@
+# libraries
+from Bio import Entrez
+from Bio import SeqIO
+
+Entrez.email = 'hans@orikami.nl'
+h = Entrez.einfo()
+record = Entrez.read(h)
+h = Entrez.esearch(
+    db='nucleotide',
+    term='Wuhan-Hu-1'
+)
+result = Entrez.read(h)
+
+with Entrez.efetch(
+    db="nucleotide", rettype="fasta", retmode="text", id=result['IdList'][0]
+) as handle:
+    seq_record = SeqIO.read(handle, "fasta")
+
+
+h.close()
+print('finished')
